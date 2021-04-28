014af3449176abd537a8e892e5ea4d025094c2fc
wandji <wbertrand10@gmail.com>
2021-04-28 23:44:50 +0100

    Set watch mode and fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   module: {
     rules: [

a20caeead1f13caf6fb7ca6672687e5b0eb35979
wandji <wbertrand10@gmail.com>
2021-04-29 05:03:16 +0100

    Add nav and styles

--- src/nav.js
+++ src/nav.js
@@ -6,4 +6,18 @@ export default function displayNav(){
   const nav = body.insertBefore(document.createElement('nav'), content)
   const logo = nav.appendChild(document.createElement('img'))
   logo.src = "../assets/logo.png"
+
+  const form = nav.appendChild(document.createElement('form'))
+  const cityName = form.appendChild(document.createElement('input'))
+  cityName.type = 'text'
+  cityName.placeholder = 'Enter a city name'
+  const formSubmit = form.appendChild(document.createElement('input'))
+  formSubmit.type = "submit";
+  formSubmit.innerHTML = 'Search'
+  formSubmit.setAttribute('class', 'submit-btn' )
+
+  const btnContainer = nav.appendChild(document.createElement('div'))
+  const tempBtn = btnContainer.appendChild(document.createElement('button'))
+  tempBtn.setAttribute('class', 'temp-btn')
+  tempBtn.textContent = 'F / C'
 }

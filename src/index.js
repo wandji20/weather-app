e741f48ea0a032ccc9b882c3971853ae590683f5
wandji <wbertrand10@gmail.com>
2021-04-29 02:21:05 +0100

    Add methods to cycle about different temperature uni measurements

--- src/index.js
+++ src/index.js
@@ -13,8 +13,30 @@ const getWeatherInfo = (location) => {
 getWeatherInfo('LImbe');
 
 
+const kelvinToCelsius = (kelvin)=>{
+  return kelvin - 273.15 //'°C'
+}
+
+const celsuisToKelvin = (celsuis)=>{
+  return celsuis + 273.15
+}
+
+const kelvinToFahrenheit = (kelvin)=>{
+  return (kelvin - 273.15) * 9/5 + 32
+}
+
+const fahrenheitToKelvin = (fahrenheit)=>{
+  return 273.15 + (fahrenheit-32) * 5/9
+}
 
 
 const displayData = (data) =>{
-  console.log(data)
+  let temperature = kelvinToCelsius(data.main.temp);
+  // let minTemp = toCelsius(data.main.temp_min);
+  // let maxTemp = toCelsius(data.main.temp_max);
+  // console.log(temperature, minTemp, maxTemp);
+  let celsius = kelvinToCelsius(data.main.temp)
+  let fahrenheit = kelvinToFahrenheit(data.main.temp)
+  console.log(data.main.temp, celsius, fahrenheit, fahrenheitToKelvin(fahrenheit), celsuisToKelvin(celsius))
+  console.log()
 }

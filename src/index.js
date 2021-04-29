d1528745bac4f219b176929d646d51e5848282a5
wandji <wbertrand10@gmail.com>
2021-04-29 01:56:06 +0100

    Add getWeatherInfo metod to get all data for a particular city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+
+const apiKey = '17c75489c7d51e26cfe6254a64c6e232';
+
+const getWeatherInfo = (location) => {
+  location = location.toLowerCase();
+  console.log(location);
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => displayData(data))
+  .catch(err => console.log(err))
+};
+getWeatherInfo('LImbe');
+
+
+
+
+const displayData = (data) =>{
+  console.log(data)
+}
